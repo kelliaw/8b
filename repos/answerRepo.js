98d20871a08fff5ada51ed040728100979a60bd4
Kelli White Weaver <kelliwhiteweaver@gmail.com>
2022-03-24 15:58:19 -0400

    Test

---
+++ repos/answerRepo.js
@@ -0,0 +1,47 @@
+let fs = require('fs');
+
+const FILE_NAME = './assets/answers.json';
+
+let answerRepo = {
+  get: function (resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(JSON.parse(data));
+      }
+    });
+  },
+  getById: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let answer = JSON.parse(data).find(p => p.id == id);
+        resolve(answer);
+      }
+    });
+  },
+  search: function (searchObject, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let answers = JSON.parse(data);
+        // Perform search
+        if (searchObject) {
+          answers = answers.filter(
+            p => (searchObject.id ? p.id == searchObject.id : true) &&
+              (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name) >= 0 : true));
+        }
+
+        resolve(answers);
+      }
+    });
+  }
+};
+
+module.exports = answerRepo;
